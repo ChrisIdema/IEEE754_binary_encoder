8946f7d689dcaed8783a63845bbd643a6c86bb4b
Chris Idema <cidema@hedon.nl>
2024-05-29 16:30:05 +0200

    fixed implementation

--- IEEE754_binary_encoder.h
+++ IEEE754_binary_encoder.h
@@ -7,10 +7,10 @@
 extern "C" {
 #endif
 
-void IEE754_binary64_encode( double x, uint8_t out[8] );
-double IEE754_binary64_decode( uint8_t out[8] );
-void IEE754_binary32_encode( float x, uint8_t out[4] );
-float IEE754_binary32_decode( uint8_t out[4] );
+void IEE754_float64_encode( double x, uint8_t out[8] );
+double IEE754_float64_decode( uint8_t out[8] );
+void IEE754_float32_encode( float x, uint8_t out[4] );
+float IEE754_float32_decode( uint8_t out[4] );
 
 #ifdef __cplusplus
 }
